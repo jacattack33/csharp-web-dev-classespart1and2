e2c48014721da009827b17d0a604a0cf3c884cef
Jacque <jacquemueller2468@gmail.com>
2022-08-17 14:41:23 -0500

    complete

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,8 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student jacque = new Student("Jacque", 1, 1, 4.0);
+            Console.WriteLine("The Student class works! " + jacque.Name + " is a student!");
         }
     }
 }
