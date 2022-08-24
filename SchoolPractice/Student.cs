f4747983150fe22fa70355b6f495bdd8ef8796f4
Jacque <jacquemueller2468@gmail.com>
2022-08-24 11:31:22 -0500

    exercises part 2

--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -26,5 +26,61 @@ namespace SchoolPractice
         {
             nextStudentId++;
         }
+
+        public string GetGradeLevel(int credits)
+        {
+            if (credits <= 29)
+            {
+                return "freshman";
+            }
+            else if (credits <= 59)
+            {
+                return "sophomore";
+            }
+            else if (credits <= 89)
+            {
+                return "junior";
+            }
+            else
+            {
+                return "senior";
+            }
+        }
+
+        public void AddGrade(int courseCredits, double grade)
+        {
+            // Update the appropriate properties: NumberOfCredits, Gpa
+            double totalQualityScore = Gpa * NumberOfCredits;
+            totalQualityScore += courseCredits * grade;
+            NumberOfCredits += courseCredits;
+            Gpa = totalQualityScore / NumberOfCredits;
+        }
+
+        public override string ToString()
+        {
+            return Name + " (Credits: " + NumberOfCredits + ", GPA: " + Gpa + ")";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == this)
+            {
+                return true;
+            }
+
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            Student studentObj = obj as Student;
+            return StudentId == studentObj.StudentId;
+        }
     }
+
 }
